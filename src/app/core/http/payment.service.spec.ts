369e00b60d7f27eab407119636ef3c9e7101ed5a
Fernando Isaías García Aguirre <figarcia@gmail.com>
2021-09-22 13:48:41 -0500

    I add coverage

--- src/app/core/http/payment.service.spec.ts
+++ src/app/core/http/payment.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PaymentService } from './payment.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 describe('PaymentService', () => {
   let service: PaymentService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PaymentService);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ PaymentService ]
+    });
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new PaymentService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be created 2', (done: DoneFn) => {
+    const expectedHeroes: any =
+      { data: [{ id: 1, name: 'A' }, { id: 2, name: 'B' }], detail: { } };
+
+    const expectedHeroes2: any =
+      { data: [{ id: 1, name: 'A' }, { id: 2, name: 'C' }], detail: { } };
+
+    httpClientSpy.get.and.returnValue(of(expectedHeroes));
+
+    service.findAll('').subscribe(resp => {
+        expect(resp).toEqual(expectedHeroes2, 'expected heroes');
+        done();
+      },
+      done.fail
+    );
+
+
+  });
+
 });
